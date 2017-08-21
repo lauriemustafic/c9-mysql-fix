7cdd94590e9a1c31291b59f62e0cdd2a624aa269
lauriemustafic <1876441+lauriemustafic@users.noreply.github.com>
2017-08-21 20:34:51 +0100

    create mysqlfix.sh

---
+++ mysqlfix.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+function mvLogFiles () {
+    if [ -e /var/lib/mysql/ib_logfile0 ]
+    then
+        echo "found ib_logfile0 moving it to ~/workspace"
+        mv /var/lib/mysql/ib_logfile0 ~/ib_logfile0.bak
+    else
+        echo "ib_logfile0 not found, trying ib_logfile1"
+    fi
+    if [ -e /var/lib/mysql/ib_logfile1 ]
+    then
+        echo "found ib_logfile1 moving it to ~/workspace"
+        mv /var/lib/mysql/ib_logfile1 ~/ib_logfile1.bak
+    else
+        echo "ib_logfile1 not found."
+    fi
+    echo "trying to start mysql"
+    service mysql start -uroot
+    
+}
+
+function init () {
+    tail -1000 /var/log/mysql/error.log | grep -q 'ibdata files do not match the log sequence number'
+    greprc=$?
+    if [[ $greprc -eq 0 ]] ; then
+        echo Found the error. Moving forward
+        mvLogFiles
+    else
+    	echo Required error not found in the logs. Stopping here.
+    fi
+}
+
+init
